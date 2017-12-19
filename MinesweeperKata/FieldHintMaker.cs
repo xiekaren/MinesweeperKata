b209ea27e61234a63e620be3ea3d7e7271c1a1b6
Karen Xie <karen-x@outlook.com>
2017-12-19 22:35:08 +1300

    hint maker

---
+++ MinesweeperKata/FieldHintMaker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MinesweeperKata
+{
+    public class FieldHintMaker
+    {
+        public List<string> GetHints(Field field)
+        {
+            return new List<string>();
+        }
+    }
+}
