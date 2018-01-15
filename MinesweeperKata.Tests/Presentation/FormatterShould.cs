f9f282abd5e76c16f14ca20a06af62fcf28c3ce6
Karen Xie <karen-x@outlook.com>
2018-01-16 08:46:45 +1300

    format with private method to remove line breaks

--- MinesweeperKata.Tests/Presentation/FormatterShould.cs
+++ MinesweeperKata.Tests/Presentation/FormatterShould.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using MinesweeperKata.DTO;
 using MinesweeperKata.Presentation;
 using NUnit.Framework;
@@ -14,6 +15,36 @@ namespace MinesweeperKata.Tests.Presentation
         public void SetUp()
         {
             _formatter = new Formatter();
+        }
+
+
+        [Test]
+        public void FormatSingleHint()
+        {
+            var hints = new List<HintField>
+            {
+                new HintField
+                {
+                    Rows = 2, Columns = 1,
+                    FieldHints = new List<string>
+                    {
+                        "1",
+                        "*"
+                    }
+                }
+            };
+
+            var expected = new[]
+            {
+                "Field #1:",
+                "1",
+                "*"
+            };
+            var expectedFormat = Format(expected);
+
+            var actual = _formatter.FormatHints(hints);
+
+            Assert.AreEqual(expectedFormat, actual);
         }
 
         [Test]
@@ -37,26 +68,7 @@ namespace MinesweeperKata.Tests.Presentation
                         "2", "*",
                         "*", "2"
                     }
-                }
-            };
-
-            const string expected = "Field #1:\n" +
-                                    "0\n" +
-                                    "\n" +
-                                    "Field #2:\n" +
-                                    "2*\n" +
-                                    "*2";
-
-            var actual = _formatter.FormatHints(hints);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void FormatSingleHint()
-        {
-            var hints = new List<HintField>
-            {
+                },
                 new HintField
                 {
                     Rows = 1, Columns = 1,
@@ -65,27 +77,32 @@ namespace MinesweeperKata.Tests.Presentation
                         "0"
                     }
                 },
-                new HintField
-                {
-                    Rows = 2, Columns = 2,
-                    FieldHints = new List<string>
-                    {
-                        "2", "*",
-                        "*", "2"
-                    }
-                }
             };
 
-            const string expected = "Field #1:\n" +
-                                    "0\n" +
-                                    "\n" +
-                                    "Field #2:\n" +
-                                    "2*\n" +
-                                    "*2";
+            var expected = new[]
+            {
+                "Field #1:",
+                "0",
+                "",
+                "Field #2:",
+                "2*",
+                "*2",
+                "",
+                "Field #3:",
+                "0"
+
+            };
+            var expectedFormat = Format(expected);
 
             var actual = _formatter.FormatHints(hints);
 
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedFormat, actual);
+        }
+
+        private static string Format(IEnumerable<string> input)
+        {
+            var formattedInput = input.Aggregate("", (current, line) => current + line + "\n");
+            return formattedInput.TrimEnd('\n');
         }
     }
 }
