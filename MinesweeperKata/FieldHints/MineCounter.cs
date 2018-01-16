c92d677084fd8f70e8cebd572c8c07528c84a965
Karen Xie <karen-x@outlook.com>
2018-01-17 08:36:00 +1300

    extract getting neighbours to another class

--- MinesweeperKata/FieldHints/MineCounter.cs
+++ MinesweeperKata/FieldHints/MineCounter.cs
@@ -6,29 +6,17 @@ namespace MinesweeperKata.FieldHints
 {
     public class MineCounter
     {
+        private readonly FieldAnalyser _fieldAnalyser;
+
+        public MineCounter()
+        {
+            _fieldAnalyser = new FieldAnalyser();
+        }
+
         public int CountAroundPoint(Point point, Field field)
         {
-            var minRow = Math.Max(0, point.Row - 1);
-            var maxRow = Math.Min(field.Rows, point.Row + 1);
-            var minColumn = Math.Max(0, point.Column - 1);
-            var maxColumn = Math.Min(field.Columns, point.Column + 1);
-
-            var mineCount = 0;
-
-            for (var rowIndex = minRow; rowIndex <= maxRow; rowIndex++)
-            {
-                for (var columnIndex = minColumn; columnIndex <= maxColumn; columnIndex++)
-                {
-                    if (rowIndex == point.Row && columnIndex == point.Column) continue;
-
-                    if (field.Locations.Any(x => x.Row == rowIndex && x.Column == columnIndex && x.IsMine))
-                    {
-                        mineCount++;
-                    }
-                }
-            }
-
-            return mineCount;
+            var neighbours = _fieldAnalyser.GetNeighbours(point, field);
+            return neighbours.Count(neighbour => neighbour.IsMine);
         }
     }
 }
